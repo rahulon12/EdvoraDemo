a0ccfb579e397a084dbf6e74b2fc385b96f2ccf3
Rahul Narayanan <rahulon@icloud.com>
2022-01-14 13:08:35 -0500

    Initial Commit

---
+++ EdvoraInternshipApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EdvoraInternshipApp
+//
+//  Created by Rahul Narayanan on 1/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EdvoraInternshipApp/EdvoraInternshipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraInternshipAppApp.swift
+//  EdvoraInternshipApp
+//
+//  Created by Rahul Narayanan on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraInternshipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
